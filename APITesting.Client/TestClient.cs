2450cd52f496402c4d811b12bebd3d16ca2b0810
Bart Spaans <143680439+spaansba@users.noreply.github.com>
2024-04-17 15:26:49 +0200

    Revert till here

---
+++ APITesting.Client/TestClient.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace APITesting.Client;
+
+public class TestClient
+{
+    /// <summary>
+    /// Warning: Never Dispose the HTTPClient
+    /// </summary>
+    private readonly HttpClient client;
+    public const string HttpClientName = "TestClient"; 
+    public TestClient(HttpClient client)
+    {
+        this.client = client;
+    }
+    
+    [ActivatorUtilitiesConstructor] //DI frameworks will use this ctor
+    public TestClient(IHttpClientFactory clientFactory)
+        : this(clientFactory.CreateClient(HttpClientName))
+    {
+
+    }
+
+    public TestClient(IHttpClientFactory clientFactory, Func<IHttpClientFactory, HttpClient> createClient)
+        : this(createClient(clientFactory))
+    {
+
+    }
+}
